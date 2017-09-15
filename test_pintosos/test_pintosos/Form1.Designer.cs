779abfcd4662566c80de873ccf47ee91e6009999
Arkagami <mixno20@gmail.com>
2017-09-15 18:02:16 +0300

    Is working.

---
+++ test_pintosos/test_pintosos/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace test_pintosos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tex = new System.Windows.Forms.TextBox();
+            this.but = new System.Windows.Forms.Button();
+            this.outfile = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(474, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // tex
+            // 
+            this.tex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tex.Location = new System.Drawing.Point(57, 31);
+            this.tex.Multiline = true;
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(367, 85);
+            this.tex.TabIndex = 2;
+            this.tex.Text = "Введите полный путь до папки pintos (например, для пользователя user с пинтосом в" +
+    " домашнем каталоге  путь - /home/user/";
+            this.tex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // but
+            // 
+            this.but.Location = new System.Drawing.Point(138, 122);
+            this.but.Name = "but";
+            this.but.Size = new System.Drawing.Size(204, 41);
+            this.but.TabIndex = 1;
+            this.but.Text = "Подтвердить";
+            this.but.UseVisualStyleBackColor = true;
+            this.but.Click += new System.EventHandler(this.but_Click);
+            // 
+            // outfile
+            // 
+            this.outfile.Enabled = false;
+            this.outfile.Location = new System.Drawing.Point(138, 169);
+            this.outfile.Name = "outfile";
+            this.outfile.Size = new System.Drawing.Size(204, 36);
+            this.outfile.TabIndex = 4;
+            this.outfile.Text = "Получить файл";
+            this.outfile.UseVisualStyleBackColor = true;
+            this.outfile.Click += new System.EventHandler(this.outfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 217);
+            this.Controls.Add(this.outfile);
+            this.Controls.Add(this.but);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pintosos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.TextBox tex;
+        private System.Windows.Forms.Button but;
+        private System.Windows.Forms.Button outfile;
+    }
+}
+
---
+++ test_pintosos/test_pintosos/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace test_pintosos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int outs = 0, elem = 0;
+        private string path;
+        private string[] tests = new string[100];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outfile_Click(object sender, EventArgs e)
+        {
+            if(outs != 0)
+            {
+                StreamWriter f = new StreamWriter("test_lab.txt");
+                f.Write("cd ");
+                f.Write(path);
+                f.WriteLine("pintos/src/threads");
+                f.WriteLine("make");
+                f.WriteLine();
+                f.WriteLine("cd build");
+                f.Write("make clean");
+                f.WriteLine();
+                for (int i = 0; i < elem; i++)
+                {
+                    f.Write("make tests/threads/");
+                    f.Write(tests[i]);
+                    f.Write(".rezult");
+                    f.WriteLine();
+                }
+                f.WriteLine();
+                f.Write("echo \"----------------------------------------------------------------------------\\n");
+                f.Write(tests[0]);
+                f.Write("\" >");
+                f.Write(path);
+                f.Write("Output.txt\ncat  tests/threads/");
+                f.Write(tests[0]);
+                f.Write(".result > ");
+                f.Write(path);
+                f.WriteLine("Output.txt");
+                for (int i = 1; i < elem; i++)
+                {
+                    f.Write("echo \"----------------------------------------------------------------------------\\n");
+                    f.Write(tests[i]);
+                    f.Write("\" >>");
+                    f.Write(path);
+                    f.Write("Output.txt\ncat  tests/threads/");
+                    f.Write(tests[i]);
+                    f.Write(".result >> ");
+                    f.Write(path);
+                    f.WriteLine("Output.txt");
+                }
+                f.WriteLine();
+                f.Write("echo \"\\n----------------------------------------------------------------------------\\n-----------------------------TESTS FINISHED---------------------------------\\n----------------------------------------------------------------------------\\n\"");
+                f.WriteLine();
+                f.WriteLine();
+                f.Write("cd ");
+                f.Write(path);
+                f.WriteLine();
+                f.Write("cat Output.txt");
+                f.Close();
+                Process.Start("test_lab.txt");
+            }            
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter f = new StreamWriter("Readme.txt");
+            f.WriteLine("Вводите полный путь до папки с пинтосом с написанием в конце знака \"\\\"(слеш) и без указания самой папки pintos.");
+            f.WriteLine("Если pintos в домашней папке - /home/user/.");
+            f.WriteLine("Потом без пробелов вводите имена тестов (alarm-single, например)");
+            f.WriteLine("Когда введы все имена тестов, нажмите кнопку Получить файл.");
+            f.WriteLine("Загрузите полученный код из файла test_lab.txt в любой файл на linux'e.");
+            f.WriteLine("В папке с этим файлом щелкните правой кнопкой мыши, откройте терминал и напишите 'sh имя_файла'.");
+            f.WriteLine("Без кавычек, соответсвенно. Это запустит тестирование pintos'a.");
+            f.WriteLine("По завершению тестирования внизу под надписью TESTS FINISHED будут результаты всех тестов с их названиями сверху.");
+            f.Close();
+            Process.Start("Readme.txt");
+        }
+
+        private void but_Click(object sender, EventArgs e)
+        {
+            if (outs == 1)
+            {
+                tests[elem++] = tex.Text;
+                tex.Text = "Введите еще одно имя теста и нажмите \"Подтвердить\" или получите файл";
+            }
+            if (outs == 0) {
+                outfile.Enabled = true;
+                path = tex.Text;
+                tex.Text = "Введите имя теста и нажмите \"Подтвердить\"";
+                outs++;
+            }
+            
+        }
+    }
+}
